8fdeb9fd92f39742fb1911d0ab73c6114e456253
chenq <346285234@qq.com>
2014-06-05 23:06:36 +0800

    first commit

---
+++ Python_Code.py
@@ -0,0 +1,65 @@
+#print 'hello, world!'
+#print 'the quick brown fox', 'jumps over', 'the lazy dog'
+#print '100+200 =', 100+200 
+#name = raw_input("please enter your name: ")
+#print 'hello', name
+
+#print absolute value of an integer:
+#a = 100
+#if a>=0:
+#    print a 
+#else:
+#    print -a
+
+#print 'I\'m ok.'
+#print 'I\'m learning\t python.'
+#print '\\\n\\'
+#print r'\\\n\\'
+#print '''line1
+#line2
+#line3'''
+
+#if True and False:
+#    print "qian"
+#else:
+#    print "ting"
+#
+#a = 123 #a is an integer
+#print a
+#a = "abc" #a is a string
+#print a
+
+#print ord('A')
+#print chr(65)
+#print u'\u4e2d'
+
+#print "hello, %s" % 'world'
+#print "%2d-%02d" % (3,1)
+#print "%.2f" % 3.1415926
+#print "Age: %s.Gender: %s" % (25, True)
+#print "%s %%" % '3'
+#print "%d %%" % 3
+#print b"abc"
+
+#classmates = ['qian', 'ting']
+#print classmates
+#print len(classmates)
+#print classmates[-1]
+#print classmates[-2]
+#classmates.append('father')
+#classmates.insert(1, 'mother')
+#print classmates
+#print "pop():"
+#classmates.pop()
+#print classmates
+
+python = []
+print len(python)
+ruby = ['hello', 'world']
+python = ['hello', 123, ruby, False]
+print python[2][1]
+
+Ruby = ('hello', 'world', python)
+print Ruby
+Ruby[2][2][0] = 'die'
+print Ruby
