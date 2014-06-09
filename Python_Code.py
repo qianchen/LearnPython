9daee3c74f2399710e337356ed51be534d7c52bc
chenq <346285234@qq.com>
2014-06-09 22:23:12 +0800

    Function and Advance behavior

--- Python_Code.py
+++ Python_Code.py
@@ -99,3 +99,66 @@
 #print year2
 #print year1 & year2
 #print year1 | year2
+
+
+#Function
+#print "Compare 1 to 3"
+#print cmp(1, 3)
+#print "The integer of 3.1"
+#print int(3.1)
+#a = int
+#print a(3.1)
+#def my_abs(x):
+#    if x>= 0:
+#        pass
+#    else:
+#        return -x
+#
+#print my_abs(1)
+#def Test(a = []):
+#    print a
+#    a.append('End')
+#    return a
+#
+#print Test()
+#print Test()
+#def Fina(*numb):
+#    sum = 0
+#    for n in numb:
+#        sum += n*n
+#    return sum
+#
+#print "1*1 + 2*2 + 3*3="
+#print Fina(1,2,3)
+#def Ma(**numb):
+#    print numb
+#numb = {'he': 1, 'she': 2}
+#Ma(**numb)
+#def AllParm(a,b,c=0,*tu,**di):
+#    print 'a =', a, 'b =', b, 'c =', c, 'tu =', tu, 'di =', di
+#
+#AllParm(1,2,3,'a','b',A=10)
+#def DiGui(numb):
+#    return TailAcc(numb, 1)
+#
+#def TailAcc(numb, accumu):
+#    if numb == 1:
+#        return accumu
+#    return TailAcc(numb - 1, numb * accumu)
+#
+#print "5!="
+#print DiGui(5)
+
+
+#Advance behavior
+#L = [1,2,3,4,5,6,7]
+#print L[:4:2] 
+#for i,value in enumerate([1,2,3,4]):
+#    print i,value
+#import os
+#print [d for d in os.listdir('.')]
+#d = {'A': 1, 'B': 2}
+#print [key + '=' + 'value' for key, value in d.iteritems()]
+#L = ['Hello', 'World', 18, 'Apple', None]
+#K = [s.lower() for s in L if isinstance(s, str)]
+#print K
